46f63d3a700013d63e01218bba8148b00b262812
Abdulkareem Abdulquddus Olayinka <aabdulqudduss@gmail.com>
2023-10-12 03:07:53 +0100

    Update index.js

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -11,6 +11,7 @@ import PageBox from './PageBox';
 import BottomNav from './nav/BottomNav';
 import Notification from './Notification';
 import Actions from './Actions';
+import Modal from './Modal';
 import AdminCard from './AdminCard';
 import FormErrors from '../components/forms/FormErrors';
 import InputField from './InputField';
@@ -20,5 +21,8 @@ import EditProfileForm from './forms/EditProfileForm';
 import AdminPaymentManagement from './PaymentManagement';
 import HeaderComponent from './HeaderComponent';
 import AddPostForm from './forms/AddPostForm';
+import AddPaymentForm from './forms/AddPaymentForm';
+import EditPaymentForm from './forms/EditPaymentForm';
+import DeletePaymentForm from './forms/DeletePaymentForm';
 
-export { FormErrors, InputField, NavBar, Footer, SignInForm, SignUpForm, Payment, Button, Sidebar, AdminPaymentManagement, Post, PageBox, AnnouncementContainer, BottomNav, Notification, Actions, AdminCard, Loader, PrivateRoutes, EditProfileForm, HeaderComponent, AddPostForm };
+export { FormErrors, InputField, NavBar, Modal, Footer, SignInForm, SignUpForm, Payment, Button, Sidebar, AdminPaymentManagement, Post, PageBox, AnnouncementContainer, BottomNav, Notification, Actions, AdminCard, Loader, PrivateRoutes, EditProfileForm, HeaderComponent, AddPostForm, AddPaymentForm, EditPaymentForm, DeletePaymentForm };
