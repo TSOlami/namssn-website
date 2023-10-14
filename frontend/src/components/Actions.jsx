89133b7c83e3aa6d2dab6398183b89c727aad087
Ifedolapoomoniyi <ifedolapo0001@gmail.com>
2023-10-14 12:30:15 +0100

    Add link to comments

--- frontend/src/components/Actions.jsx
+++ frontend/src/components/Actions.jsx
@@ -1,44 +1,120 @@
-import { BiDownvote, BiShareAlt, BiSolidDownvote, BiSolidUpvote, BiUpvote } from "react-icons/bi"
+import {
+	BiComment,
+	BiDownvote,
+	BiShareAlt,
+	BiSolidDownvote,
+	BiSolidUpvote,
+	BiUpvote,
+} from "react-icons/bi";
+import { useNavigate } from "react-router-dom";
 
-const Actions = ({ upvotes, downvotes, shares, isUpvoted, onUpvote, isDownvoted, onDownvote }) => {
-  return (
-    <div className="py-4 flex flex-row justify-start gap-20 pr-4 items-center">
-    <div>
-      <span className="flex flex-row items-center gap-1">
-        {isUpvoted ? (
-          <button onClick={onUpvote}>
-              <BiSolidUpvote color="#17A1FA" /> {upvotes}{" "}
-          </button>
-        ) : (
-          <button onClick={onUpvote}>
-            <BiUpvote /> {upvotes}{" "}
-          </button>
-        )}
-      </span>
-      <span>Upvotes</span>
-    </div>
-    <div>
-      <span className="flex flex-row items-center gap-1">
-        {isDownvoted ? (
-          <button onClick={onDownvote}>
-            <BiSolidDownvote color="red" /> {downvotes}{" "}
-          </button>
-        ) : (
-          <button onClick={onDownvote}>
-            <BiDownvote /> {downvotes}{" "}
-          </button>
-        )}
-      </span>
-      <span>Downvotes</span>
-    </div>
-    <div>
-      <span className="flex items-center gap-1">
-        <BiShareAlt /> {shares}
-      </span>
-      <span>Share</span>
-    </div>
-  </div>
-  )
-}
+const Actions = ({
+	upvotes,
+	downvotes,
+	comments,
+	shares,
+	isUpvoted,
+	onUpvote,
+	isDownvoted,
+	onDownvote,
+	postId,
+}) => {
+	const navigate = useNavigate();
+	const routeToComments = () => {
+		navigate(`/comments/${postId}`);
+	};
 
-export default Actions
+	// const [openComment, setOpencomment] = useState(false);
+	// const handleOpenComment = () => {
+	// 	setOpencomment(!openComment);
+	// };
+	// const location = useLocation();
+	return (
+		<div className="py-4 flex flex-row justify-start gap-20 pr-4 items-center">
+			<div>
+				<span className="flex flex-row items-center gap-1">
+					{isUpvoted ? (
+						<button
+							onClick={onUpvote}
+							className="flex flex-row items-center gap-1"
+						>
+							<BiSolidUpvote color="#17A1FA" />
+							<span>{upvotes} </span>
+						</button>
+					) : (
+						<button
+							onClick={onUpvote}
+							className="flex flex-row items-center gap-1"
+						>
+							<BiUpvote /> {upvotes}{" "}
+						</button>
+					)}
+				</span>
+				<span>Upvotes</span>
+			</div>
+			<div>
+				<span className="flex flex-row items-center gap-1">
+					{isDownvoted ? (
+						<button
+							onClick={onDownvote}
+							className="flex flex-row items-center gap-1"
+						>
+							<BiSolidDownvote color="red" /> {downvotes}{" "}
+						</button>
+					) : (
+						<button
+							onClick={onDownvote}
+							className="flex flex-row items-center gap-1"
+						>
+							<BiDownvote /> {downvotes}{" "}
+						</button>
+					)}
+				</span>
+				<span>Downvotes</span>
+			</div>
+
+			<div>
+				<span className="flex flex-row items-center gap-1">
+					<button
+						className="flex flex-row items-center gap-1"
+						onClick={routeToComments}
+					>
+						<BiComment /> {comments}
+					</button>
+				</span>
+				<span>Comments</span>
+			</div>
+
+			<div>
+				<span className="flex items-center gap-1">
+					<BiShareAlt /> {shares}
+				</span>
+				<span>Share</span>
+			</div>
+
+			{/* {location.pathname !== `/comments/${postId}` && (
+				<div>
+					<span className="flex flex-row items-center gap-1">
+						<button
+							className="flex flex-row items-center gap-1"
+							onClick={routeToComments}
+						>
+							<BiComment /> {comments}
+						</button>
+					</span>
+					<span>Comments</span>
+				</div>
+			)}
+			{location.pathname !== `/comments/${postId}` && (
+				<div>
+					<span className="flex items-center gap-1">
+						<BiShareAlt /> {shares}
+					</span>
+					<span>Share</span>
+				</div>
+			)} */}
+		</div>
+	);
+};
+
+export default Actions;
