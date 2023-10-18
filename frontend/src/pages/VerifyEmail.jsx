eea770a734c610c83874587a2a468d9d7d4f3974
Ifedolapoomoniyi <ifedolapo0001@gmail.com>
2023-10-18 22:13:00 +0100

    Email Verification page

---
+++ frontend/src/pages/VerifyEmail.jsx
@@ -0,0 +1,20 @@
+import { VerifyEmailSVG } from "../assets"
+import { VerificationCodeInput } from "../components"
+const VerifyEmail = () => {
+  return (
+    <div className="flex flex-col items-center justify-center gap-6 h-screen">
+      <div>
+        <img src={VerifyEmailSVG} alt="" className="h-[200px]"/>
+      </div>
+
+      <div className="flex items-center flex-col">
+        <h1 className="text-3xl font-bold">Verify your email</h1>
+        <p className="p-2">Check your email for the six digit code sent to the email entered</p>
+
+        <VerificationCodeInput codeLength={4}/>
+      </div>
+    </div>
+  )
+}
+
+export default VerifyEmail
