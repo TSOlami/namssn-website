9b5beb7a28b6e6967a2cea552f65004b36dcb9ad
TSOlami <tijani.saheed@yahoo.com>
2023-10-21 17:59:06 +0100

    fix loader

--- frontend/src/components/PaymentList.jsx
+++ frontend/src/components/PaymentList.jsx
@@ -1,7 +1,8 @@
 import { useAllPaymentsQuery, setCategories } from "../redux"; // Import Redux Toolkit Query hook and action creator
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { HeaderComponent } from "../components";
+import { HeaderComponent, Loader } from "../components";
+import { ErrorPage } from "../pages";
 
 const PaymentList = () => {
 	const { userInfo } = useSelector((state) => state.auth);
@@ -25,9 +26,9 @@ const PaymentList = () => {
 							Payments to be Made
 						</h1>
 						{isLoading ? (
-							<p>Loading...</p>
+							<Loader />
 						) : isError ? (
-							<p>Error loading payments</p>
+							<ErrorPage />
 						) : !payments ||
 				!Array.isArray(payments) || payments.length === 0 ? (
 							<p>No payments to be made at the moment.</p>
