797269d37c93eca460763aa176f7702e962d92d7
Ifedolapoomoniyi <ifedolapo0001@gmail.com>
2023-10-23 07:32:25 +0100

    Style component on select

--- frontend/src/components/AdminEventsCard.jsx
+++ frontend/src/components/AdminEventsCard.jsx
@@ -1,8 +1,36 @@
+import { useEffect } from "react";
 import { AiFillCaretRight } from "react-icons/ai";
 
 const AdminEventsCard = ({ title, flier }) => {
+	useEffect(() => {
+		const clickHandler = (event) => {
+			document
+				.querySelectorAll("#event-card")
+				.forEach((card) => card.classList.remove("border-l-4"));
+			event.target.classList.add("rounded-l-none");
+			event.target.classList.add("ml-2");
+			event.target.classList.add("border-l-4");
+			event.target.classList.add("border-primary");
+			event.target.classList.add("rounded-l-none");
+		};
+	
+		document.querySelectorAll("#event-card").forEach((card) => {
+			card.addEventListener("click", clickHandler);
+		});
+	
+		// Define the cleanup function
+		const cleanup = () => {
+			document.querySelectorAll("#event-card").forEach((card) => {
+				card.removeEventListener("click", clickHandler);
+			});
+		};
+	
+		// Return the cleanup function
+		return cleanup;
+	}, []);
+	
 	return (
-		<div className="text-black bg-greyish flex gap-2 rounded-lg p-2 px-4 m-4 items-center cursor-pointer hover:border-l-4 hover:ml-2 hover:border-primary hover:rounded-l-none">
+		<div className="text-black bg-greyish flex gap-2 rounded-lg p-2 px-4 m-4 items-center cursor-pointer hover:scale-105 active:scale-95" id="event-card">
 			<div>
 				<img src={flier} alt="" />
 			</div>
