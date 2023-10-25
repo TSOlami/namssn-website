83ca9d890989a3da370a35ad65f18224bccf93da
Ifedolapoomoniyi <ifedolapo0001@gmail.com>
2023-10-25 13:10:34 +0100

    Remove edit blog option

--- frontend/src/components/BlogCard.jsx
+++ frontend/src/components/BlogCard.jsx
@@ -1,4 +1,4 @@
-const BlogCard = ({ blog, editBlog, deleteBlog }) => {
+const BlogCard = ({ blog, deleteBlog }) => {
 	return (
 		<div className="flex flex-row justify-between px-2 shadow-md m-4 items-center">
 			<div className="mr-4 h-2 w-2 rounded-full bg-primary"></div>
@@ -6,7 +6,6 @@ const BlogCard = ({ blog, editBlog, deleteBlog }) => {
       <div className="font-medium pr-5 mr-auto">{blog.title}</div>
 
       <div className="flex flex-row gap-3">
-        <button onClick={editBlog} className="p-2 border rounded-md text-md font-medium">Edit Post</button>
         <button onClick={deleteBlog} className="p-2 border rounded-md text-md font-medium text-white bg-red-500">Delete Post</button>
       </div>
 		</div>
