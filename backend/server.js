9f5736d8ed3999978b0a3890a681772db8f504ca
Abdulkareem Abdulquddus Olayinka <aabdulqudduss@gmail.com>
2023-11-02 13:55:37 +0100

    Revert "Update server.js"
    This reverts commit e3c1908c2dbcded471d149b268dea0645e86bf1b.

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import memjs from 'memjs';
 import connectDb from './config/db.js';
-import path from 'path'
+
 import createServer from './utils/server.js';
 
 dotenv.config();
@@ -14,23 +14,7 @@ const port = process.env.PORT || 5000;
 // Create a memjs client
 export const client = memjs.Client.create();
 
-// const app = createServer();
-const app = express();
-// ---------------- deployment-----------------------
-const __dirname1=path.resolve();
-if (process.env.NODE_ENV ==='production') {
-  app.use(express.static(path.join(__dirname1, '/frontend/dist')));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname1,'frontend','dist','index.html'));
-  });
-  
-} else {
-  app.get("/",(req,res)=>{
-    res.send('API is running');
-  });
-}
-//-------------deployment--------------
+const app = createServer();
 
 app.listen(port, async () => {
   try {
