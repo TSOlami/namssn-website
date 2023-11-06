b517ab5a6805de566deb0267f4e63f14445a9bf6
Abdulkareem Abdulquddus Olayinka <aabdulqudduss@gmail.com>
2023-11-07 00:58:49 +0100

    Revert "Update vite.config.js"
    This reverts commit f6cb804e32c2c330b74079d836b88df5566ddce4.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
 		port: 3000,
 		proxy: {
 			"/api/v1": {
-				target: 'https://namssnbackend.onrender.com',
+				target: 'http://localhost:5000',
 				changeOrigin: true,
 			},
 		},
