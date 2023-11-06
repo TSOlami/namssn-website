f6cb804e32c2c330b74079d836b88df5566ddce4
Abdulkareem Abdulquddus Olayinka <aabdulqudduss@gmail.com>
2023-11-07 00:43:50 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
 		port: 3000,
 		proxy: {
 			"/api/v1": {
-				target: 'http://localhost:5000',
+				target: 'https://namssnbackend.onrender.com',
 				changeOrigin: true,
 			},
 		},
