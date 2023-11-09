8feacba196685fca6b350b79512fa2d8e4507ee8
TSOlami <tijani.saheed@yahoo.com>
2023-11-09 15:04:48 +0100

    Setup editing and deleting events

--- frontend/src/pages/AdminEvents.jsx
+++ frontend/src/pages/AdminEvents.jsx
@@ -1,15 +1,39 @@
+import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import {
 	AdminEventsCard,
 	EventForm,
 	HeaderComponent,
 	Sidebar,
+	Loader,
 } from "../components";
-import { mockEvents } from "../data";
+import { useDispatch } from "react-redux";
+import { useAllEventsQuery, setEvents } from "../redux";
 
 
 const AdminEvents = () => {
-	
+	// Fetch all announcements
+	const { data: events, isLoading: isFetching } = useAllEventsQuery();
+
+	// State to manage selected event
+	const [selectedEvent, setSelectedEvent] = useState(null);
+
+	// Create a dispatch function
+	const dispatch = useDispatch();
+
+	// Handle data fetching and state updates
+	useEffect(() => {
+		if (events) {
+			dispatch(setEvents(events));
+		}
+	}, [dispatch, events]);
+
+	const handleSelectChange = (event) => {
+    const selectedTitle = event.target.value;
+    const selectedEvent = events.find((event) => event.title === selectedTitle);
+    setSelectedEvent(selectedEvent);
+  };
+
 	return (
 		<motion.div
 			initial={{ opacity: 0, x: 100 }}
@@ -23,7 +47,7 @@ const AdminEvents = () => {
 				<div className="w-full flex md:flex-row flex-col">
 					{/* Events section */}
 					<div className="flex-1 md:block hidden">
-						{mockEvents.map((event, index) => (
+						{events?.map((event, index) => (
 							<AdminEventsCard
 								key={index}
 								title={event.title}
@@ -46,8 +70,14 @@ const AdminEvents = () => {
 							name=""
 							id=""
 							className="bg-black text-white w-[50%] p-2 rounded-lg"
+							onChange={handleSelectChange}
 						>
-							{mockEvents.map((event, index) => (
+							<option value="" disabled>
+              Select an event or create a new one
+              </option>
+							{/* Add an option for creating a new event */}
+							<option value="createNewEvent">Create New Event</option>
+							{events?.map((event, index) => (
 								<option value={event.title} key={index}>
 									{event.title}
 								</option>
@@ -57,8 +87,13 @@ const AdminEvents = () => {
 
 					{/*Event form */}
 					<div className="flex-1 w-full border-gray-300 border-l-2 h-full">
-						<EventForm />
+						<EventForm
+							selectedOption={selectedEvent}
+						/>
 					</div>
+					{/* End of event form */}
+
+					{ isFetching && <Loader />}
 				</div>
 			</div>
 		</motion.div>
