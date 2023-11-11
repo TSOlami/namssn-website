5854d900e4dbe91cfda32b82001fec4251b77fcb
Ifedolapoomoniyi <ifedolapo0001@gmail.com>
2023-11-11 06:16:45 +0100

    Edit image height

--- frontend/src/components/Team.jsx
+++ frontend/src/components/Team.jsx
@@ -4,20 +4,20 @@ const Team = ({ image, facebook, linkedin, name, position }) => {
 	return (
 		<div className="flex flex-col w-[250px] relative gap-3 m-5 shadow-2xl">
 			<div>
-				<img src={image} alt="" className="w-[250px] h-[180px] object-cover rounded-2xl"/>
+				<img src={image} alt="" className="w-[250px] h-[250px] object-cover rounded-2xl"/>
 			</div>
 
 
       {/* empty styling div */}
-      <div className="absolute block h-[65px] w-[114px] bg-white top-[127px]">
+      <div className="absolute block h-[65px] w-[114px] bg-white top-[197px]">
       </div>
 
       {/* empty styling div 2 */}
 
-      <div className="absolute block h-[65px] w-[18px] bg-white top-[127px] right-[127px] rounded-xl rounded-br-none z-20">
+      <div className="absolute block h-[65px] w-[18px] bg-white top-[197px] right-[127px] rounded-xl rounded-br-none z-20">
       </div>
 
-			<div className="bg-black flex flex-row items-center justify-center text-xl gap-4 text-blue-400 absolute w-28 top-[120px] h-14 mt-5 rounded-tl-2xl pr-4">
+			<div className="bg-black flex flex-row items-center justify-center text-xl gap-4 text-blue-400 absolute w-28 top-[190px] h-14 mt-5 rounded-tl-2xl pr-4">
 				<a href={facebook} className="hover:scale-125">
 					<img src={FacebookIcon} alt="" />
 				</a>
