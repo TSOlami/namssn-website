7762b530599049eb1e8b8f5f6e7d7c539a944b56
TSOlami <tijani.saheed@yahoo.com>
2023-11-28 12:33:16 +0100

    Remove Payment Feature

--- frontend/src/pages/PaymentPage.jsx
+++ frontend/src/pages/PaymentPage.jsx
@@ -1,11 +1,23 @@
 // import PaymentForm from "../components/Payment";
 import { PaymentList, Sidebar } from "../components";
 const PaymentPage = () => {
+	const isFeatureIsActive = false;
 	return (
 		<div className="flex flex-row">
 			<Sidebar />
 			<div className="flex flex-row w-full h-full ">
-				<PaymentList />
+				{isFeatureIsActive ? (
+					<PaymentList />
+				) : (
+					<div className="flex flex-col items-center justify-center w-full h-full">
+						<h1 className="text-3xl font-semibold text-gray-700">
+							This feature is not available yet
+						</h1>
+						<p className="text-xl font-medium text-gray-500">
+							Please check back later
+						</p>
+					</div>
+				)}
 			</div>
 		</div>
 	);
