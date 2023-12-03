508c0d6d7866640cea60ddb85b5e004cce8f240a
Ifedolapoomoniyi <ifedolapo0001@gmail.com>
2023-12-03 03:08:14 +0100

    Copy code from main

--- frontend/src/components/TechTeam.jsx
+++ frontend/src/components/TechTeam.jsx
@@ -4,26 +4,18 @@ const TechTeam = ({ image, linkedin, github, website, name, position }) => {
 	return (
 		<div className="flex flex-col w-[250px] relative gap-3 m-5 shadow-2xl">
 			<a href={website}>
-				<img
-					src={image}
-					alt=""
-					className="w-[250px] h-[250px] object-cover rounded-2xl"
-				/>
+			<img src={image} alt="" className="w-[250px] h-[250px] object-cover rounded-2xl"/>
 			</a>
 
-			{/* empty styling div */}
-			<div className="absolute block h-[65px] w-[114px] bg-white top-[197px]"></div>
 
-			{/* empty styling div 2 */}
+      {/* empty styling div */}
+      <div className="absolute block h-[65px] w-[114px] bg-white top-[197px]">
+      </div>
 
-			<div className="absolute block h-[65px] w-[18px] bg-white top-[197px] right-[127px] rounded-xl rounded-br-none z-20"></div>
+      {/* empty styling div 2 */}
 
-			<div className="bg-black flex flex-row flex-wrap items-center justify-center text-xl text-blue-400 absolute w-28 top-[190px] h-14 mt-5 rounded-tl-2xl pr-3 p-2 gap-x-4 gap-2">
-				{facebook && (
-					<a href={facebook} className="hover:scale-125">
-						<img src={FacebookIcon} alt="facebook" />
-					</a>
-				)}
+      <div className="absolute block h-[65px] w-[18px] bg-white top-[197px] right-[127px] rounded-xl rounded-br-none z-20">
+      </div>
 
 			<div className="bg-black flex flex-row items-center justify-center text-xl gap-4 text-blue-400 absolute w-28 top-[190px] h-14 mt-5 rounded-tl-2xl pr-4">
 				<a href={github} className="hover:scale-125">
@@ -37,10 +29,10 @@ const TechTeam = ({ image, linkedin, github, website, name, position }) => {
 				</a>
 			</div>
 
-			<div className="p-4 bg-black text-white rounded-b-2xl">
-				<div className="font-semibold">{name}</div>
-				<a href={website}>{position}</a>
-			</div>
+      <div className="p-4 bg-black text-white rounded-b-2xl">
+        <div className="font-semibold">{name}</div>
+        <a href={website}>{position}</a>
+      </div>
 		</div>
 	);
 };
